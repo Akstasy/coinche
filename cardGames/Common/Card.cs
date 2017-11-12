34dc4e6d5e98f60d35306f1b9bba23e2657194bc
Akstasy <akram.abd-ali@epitech.eu>
2017-11-12 17:02:54 +0100

    add card class

---
+++ cardGames/Common/Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Card
+    {
+        public UInt16 Sign { get; private set; }
+        public UInt16 Value { get; private set; }
+
+        public void setSign(UInt16 sign) { Sign = sign; }
+        public void setValue(UInt16 value) { Value = value; }
+
+        public Card(UInt16 sign, UInt16 value)
+        {
+            this.Sign = sign;
+            this.Value = value;
+        }
+    }
+}
